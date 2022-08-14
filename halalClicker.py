69514f3f9ff0a2ba24b097b407c2f21f17ce3315
mostafa-alqadi <109628361+mostafa-alqadi@users.noreply.github.com>
2022-08-14 18:17:19 +0100

    Final edition

--- halalClicker.py
+++ halalClicker.py
@@ -26,7 +26,9 @@ def click():
   elif count == 90:
     rank.configure(text=f"Remarkable Muslim")
   elif count == 100:
-    rank.configure(text=f"JANNAH FOR YOU BROTHER")
+    rank.configure(text=f"JANNAH FOR YOU BROTHER MASHALLAH")
+    thawab.configure(text=f"JANNAH FOR YOU BROTHER MASHALLAH") 
+    sicktitle.configure(text=f"JANNAH FOR YOU BROTHER MASHALLAH")
 
 #Initialising the window
 root = Tk()
@@ -35,16 +37,18 @@ root.title("Halal Clicker")
 root.iconbitmap("kaaba2.ico")
 photo=PhotoImage(file="kaaba.png")
 
+#Epic title
+sicktitle = Label(root, text="VERY GOOD OFFICIAL HALAL CLICKER PATENT PENDING (do not steal or jahanam awaits you)", font=("Comic Sans MS", 10),bg="tan", width=80)
+sicktitle.pack()
+
 #Thawab button
 button = Button(root,
  text="Click for thawab!!!",
  command=click,
  font=("Comic Sans MS", 30),
- fg="black",
- bg="#BDEBED",
- activebackground="#D4F4F5",
- activeforeground="black",
  image=photo,
+ bg="#BDEBED",
+ activebackground="#BDEBED",
  compound="top",
  cursor="heart",
  width=640)
@@ -52,10 +56,10 @@ button.pack()
 
 
 #Thawab counter
-thawab = Label(root,text="Thawab = 0",font=("Comic Sans MS", 20),fg="black", bg="#BDEBED", width=40)
+thawab = Label(root,text="Thawab = 0",font=("Comic Sans MS", 20),fg="black", bg="tan", width=40)
 thawab.pack()
 
 #Achievement label
-rank = Label(root,text="Foetus Muslim",font=("Comic Sans MS", 20),fg="black", bg="#BDEBED", width=40)
+rank = Label(root,text="Foetus Muslim",font=("Comic Sans MS", 20),fg="black", bg="tan", width=40)
 rank.pack()
 root.mainloop()
