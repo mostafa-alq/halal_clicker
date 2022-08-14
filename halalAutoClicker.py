165d91a0b27506a665c7cdbcb76033db592de72d
mostafa-alqadi <109628361+mostafa-alqadi@users.noreply.github.com>
2022-08-14 13:11:44 +0100

    Created a basic button and image

--- halalAutoClicker.py
+++ halalAutoClicker.py
@@ -1,1 +1,32 @@
-aaaaaa
+from tkinter import *
+
+count = 0
+def click():
+  global count
+  count +=1
+  if count == 1:
+    print("You clicked me once.")
+  else:
+    print("You clicked me:", count,"times.")
+
+window = Tk()
+
+photo=PhotoImage(file="kaaba.png")
+
+button = Button(window,
+ text = "Click for thawab.",
+ command=click,
+ font=("Comic Sans", 30),
+ fg="#00FF00",
+ bg="black",
+ activebackground="black",
+ activeforeground="#00FF00",
+ state=ACTIVE,
+ image=photo,
+ compound="top")
+button.pack()
+
+window.mainloop()
+
+
+
