1f4271bc98f2a18f267d88dea80f9f2bfad61a86
mostafa-alqadi <109628361+mostafa-alqadi@users.noreply.github.com>
2022-08-14 18:02:49 +0100

    Compiled successfully + More changes

--- halalClicker.py
+++ halalClicker.py
@@ -9,21 +9,23 @@ def click():
   thawab.configure(text=f"Thawab: {count}")
   if count == 10:
     rank.configure(text=f"Newb Muslim")
-  elif count == 25:
+  elif count == 20:
     rank.configure(text=f"Inpet Muslim")
-  elif count == 50:
+  elif count == 30:
+    rank.configure(text=f"Weak Muslim")
+  elif count == 40:
     rank.configure(text=f"Unskilled Muslim")
-  elif count == 75:
+  elif count == 50:
     rank.configure(text=f"Mediocre Muslim")
-  elif count == 100:
+  elif count == 60:
     rank.configure(text=f"Initiated Muslim")
-  elif count == 250:
+  elif count == 70:
     rank.configure(text=f"Dedicated Muslim")
-  elif count == 500:
+  elif count == 80:
     rank.configure(text=f"Skilled Muslim")
-  elif count == 750:
+  elif count == 90:
     rank.configure(text=f"Remarkable Muslim")
-  elif count == 1000:
+  elif count == 100:
     rank.configure(text=f"JANNAH FOR YOU BROTHER")
 
 #Initialising the window
@@ -44,16 +46,16 @@ button = Button(root,
  activeforeground="black",
  image=photo,
  compound="top",
- cursor="heart")
+ cursor="heart",
+ width=640)
 button.pack()
 
 
 #Thawab counter
-thawab = Label(root,text="Thawab = 0")
+thawab = Label(root,text="Thawab = 0",font=("Comic Sans MS", 20),fg="black", bg="#BDEBED", width=40)
 thawab.pack()
 
 #Achievement label
-rank = Label(root,text="Foetus Muslim")
+rank = Label(root,text="Foetus Muslim",font=("Comic Sans MS", 20),fg="black", bg="#BDEBED", width=40)
 rank.pack()
-
 root.mainloop()
